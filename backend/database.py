6fe88d30eead798e76f7d5dd6f6bb96eabb6be8f
frakizzz <119675886+frakizzz@users.noreply.github.com>
2026-05-31 01:23:52 +0300

    ENDED2

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://postgres:1337@localhost:5432/task_manager"
+# Перемикаємося на автономну базу SQLite
+SQLALCHEMY_DATABASE_URL = "sqlite:///./teamtasker.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
